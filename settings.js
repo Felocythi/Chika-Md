1c12ee812384fb2c0509f21f4d563f566ef0ddcf
Felocythi <104991756+Felocythi@users.noreply.github.com>
2022-05-25 12:16:02 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6283834219877'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6283834219877'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6283834219877' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Febzz' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Flex-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Febzz' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/RiyAPIs'
-global.email = 'riychdwaynee@gmail.com'
+global.youtube = 'https://youtube.com/channel/UCB1j6WK6NVupR7a-MfHfOBg'
+global.ig = 'https://www.instagram.com/p/CXNf3AThwWM/?igshid=YmMyMTA2M2Y'
+global.mygc = 'https://chat.whatsapp.com/Bk4k1zHeRzYGO6dMMBMdG0'
+global.myweb = 'blum ada'
+global.email = 'arfebri09@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Flex-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Febzz' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
